81889e96da333a6f21be4d6936ca6b54e71c43de
Sai <rockon.limitless@gmail.com>
2018-09-20 14:33:27 +0200

    "Changes"

--- src/main/java/com/qa/test/JenkinsTest.java
+++ src/main/java/com/qa/test/JenkinsTest.java
@@ -4,13 +4,9 @@ package com.qa.test;
 public class JenkinsTest {
 
 	public static void main(String args[]){
-	
-<<<<<<< HEAD
-	
-	 int a = 10;
-=======
+
 		int a = 10;
->>>>>>> cf51cf3ed6e8ecada120d5ca91b03e07398536d3
+
 		int b = 20;
 		int c = a+b;
 		System.out.println("Sum of :"+ c);
--- src/test/java/JenkinsTesting.java
+++ src/test/java/JenkinsTesting.java
@@ -1,15 +1,15 @@
-package com.qa.test;
 
 
-public class JenkinsTest {
+public class JenkinsTesting {
 
 	public static void main(String args[]){
-	
-	
-	 int a = 10;
+
+
+		int a = 10;
 		int b = 20;
 		int c = a+b;
 		System.out.println("Sum of :"+ c);
+		System.out.println(" First Jenkins Testing Project");
 	}
-	
+
 }
